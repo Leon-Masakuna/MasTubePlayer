2c8be5b259d15fdf111120bdfa2a79f1d6f5dddc
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-22 07:28:00 +0100

    feat : create search result page

---
+++ src/pages/SearchResultPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import SearchPage from '../components/SearchPage'
+
+const SearchResultPage = () => {
+   return (
+      <div>
+         <Navbar />
+         <SearchPage />
+      </div>
+   )
+}
+
+export default SearchResultPage
