ad6ac329c825a087d8ae66fa4a9b8afc05c56e69
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-22 07:34:47 +0100

    feat : create the page for reading liked videos

--- src/pages/MainVideoReadingPage.jsx
+++ src/pages/MainVideoReadingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import VideoLecture from '../components/MainVideoReading'
+import Navbar from '../components/Navbar'
 
 const MainVideoReadingPage = () => {
    return (
       <div>
+         <Navbar />
          <VideoLecture />
       </div>
    )
