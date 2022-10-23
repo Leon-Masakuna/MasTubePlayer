787b2af1f1d9c576076f9c34b19db7f491373d26
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-23 21:20:01 +0100

    feat : create page route for video reading

---
+++ src/pages/VideoReadingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import VideoReading from '../components/VideoReading'
+
+const VideoReadingPage = () => {
+   return (
+      <div>
+         <VideoReading />
+      </div>
+   )
+}
+
+export default VideoReadingPage
