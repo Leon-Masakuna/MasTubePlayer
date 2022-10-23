b43a4eecfae08c01aa6cba42f39fd0c41c925416
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-23 20:57:44 +0100

    feat : display popular videos got from the requested api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,15 +4,21 @@ import Card from './Card'
 import { Link, useNavigate } from 'react-router-dom'
 import '../styles/main_style.css'
 import '../styles/card_style.css'
+import Loader from './Loader'
+import ErrorPage from './ErrorPage'
 
 const Main = () => {
    //state
    const [videos, setVideos] = useState([])
    const navigate = useNavigate()
+   const [loading, setLoading] = useState(true)
+   const [error, setError] = useState(false)
 
    //behavior
    const key = 'AIzaSyAjYZj_Ga7caIIP_HlQ3Qi5HmgPTG1LGVI'
-   const fecthData = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=75&myRating=like&key=${key}`
+   /*  const key = process.env.REACT_APP_CLIENT_KEY */
+
+   const fecthData = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=FR&key=${key}`
 
    const accessToken = localStorage.getItem('token')
    console.log('Main component : ', accessToken)
@@ -23,8 +29,12 @@ const Main = () => {
          headers: new Headers({ Authorization: `Bearer ${accessToken}` }),
       })
          .then((res) => res.json())
-         .then((data) => setVideos(data.items))
-   }, [])
+         .then((data) => {
+            setVideos(data?.items)
+            setLoading(false)
+         })
+         .catch(() => setError(true))
+   }, [accessToken])
 
    useEffect(() => {
       if (!accessToken) {
@@ -32,22 +42,32 @@ const Main = () => {
       }
    }, [accessToken, navigate])
 
-   console.log(videos)
+   useEffect(() => {
+      if (error) {
+         return <ErrorPage />
+      }
+   }, [accessToken])
+
+   console.log('erreur : ', error)
    //render
    return (
       <>
          <div className="grid_sidebar_searchbar">
             <div className="main_side">
                <div className="image__preview image__container">
-                  {videos.map((item, id) => (
-                     <Link
-                        className="video__link__style"
-                        to={`/videoplay/${item.id}`}
-                        key={id}
-                     >
-                        <Card key={id} video={item} />
-                     </Link>
-                  ))}
+                  {!loading ? (
+                     videos?.map((item, id) => (
+                        <Link
+                           className="video__link__style"
+                           to={`/videoplay/${item.id}/${item?.snippet?.channelId}`}
+                           key={id}
+                        >
+                           <Card key={id} video={item} />
+                        </Link>
+                     ))
+                  ) : (
+                     <Loader />
+                  )}
                </div>
             </div>
          </div>
