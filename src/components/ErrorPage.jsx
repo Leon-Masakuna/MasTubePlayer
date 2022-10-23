af756a5c3da52d647d0ef10377241a3891674f8f
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-23 21:16:09 +0100

    fix : implemente functionalities in case of error or request faillure

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+   return (
+      <div className="error">
+         <h1 className="error__phrase">Quota Quotes Exceded</h1>
+      </div>
+   )
+}
+
+export default ErrorPage
