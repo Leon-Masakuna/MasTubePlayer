d242ec98857d303bbdf03a169e02d47375968a89
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-23 21:01:07 +0100

    feat : implemente searchbar functionalities

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -19,7 +19,7 @@ const Searchbar = () => {
             onChange={handleChange}
             value={inputSearch}
          />
-         <Link className="button_link" to={`/searchpage${inputSearch}`}>
+         <Link className="button_link" to={`/searchpage/${inputSearch}`}>
             <button className="searchbar searchbar__button" type="submit">
                <i className="fa-solid fa-magnifying-glass"></i>
             </button>
