237f253dcc4abb042480de51189659248178528a
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-28 10:50:32 +0100

    feat : add video card title

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,16 @@ import React from 'react'
 import '../styles/card_style.css'
 
 const Card = ({ video }) => {
-   const chanelImage = localStorage.getItem('item')
-
    return (
       <div>
          <div className="image_space">
             <div className="card__items">
                <img
                   className="image"
-                  src={video?.snippet?.thumbnails?.medium?.url}
+                  src={video.snippet.thumbnails.medium.url}
                   alt="image video"
                />
-               <p className="video__title">{video?.snippet?.title}</p>
+               <p className="video__title">{video.snippet.title}</p>
             </div>
          </div>
       </div>
