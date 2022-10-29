ac5cf6ef3043465f02ed5bf057d968767b9e75d6
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-29 09:50:24 +0100

    feat : manage special caractors design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../styles/card_style.css'
+import numeral from 'numeral'
+import moment from 'moment/moment'
 
 const Card = ({ video }) => {
    return (
@@ -12,6 +14,18 @@ const Card = ({ video }) => {
                   alt="image video"
                />
                <p className="video__title">{video.snippet.title}</p>
+               <p className="video__title channel__title">
+                  {video.snippet.channelTitle}
+               </p>
+               <div className="video__info">
+                  <div className="comment__info">
+                     {numeral(video.statistics?.viewCount).format('O.a')} views
+                  </div>
+                  {'-'}
+                  <div className="comment__info">
+                     published : {moment(video.snippet.publishedAt).fromNow()}
+                  </div>
+               </div>
             </div>
          </div>
       </div>
