0ae13cc19f4d3e204be0413ab07f93026cecaeaa
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-29 11:34:26 +0100

    fix : add auto play functionality

--- src/components/VideoReading.jsx
+++ src/components/VideoReading.jsx
@@ -90,7 +90,7 @@ const VideoReading = () => {
                   <iframe
                      width="560"
                      height="315"
-                     src={`https://www.youtube.com/embed/${videoId}`}
+                     src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
                      title="YouTube video player"
                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                      allowFullScreen="allowFullScreen"
