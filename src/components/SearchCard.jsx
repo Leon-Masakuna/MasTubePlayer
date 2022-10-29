ccdcbb93608362ab5bc1c0097fded3a9a0d975b5
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-29 13:26:32 +0100

    fix : add react-show-more-text to title in cards

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../styles/card_style.css'
-import numeral from 'numeral'
 import moment from 'moment/moment'
+import ShowMoreText from 'react-show-more-text'
 
 const SearchCard = ({ video }) => {
    const title = video.snippet.title
@@ -16,7 +16,17 @@ const SearchCard = ({ video }) => {
                   src={video.snippet.thumbnails.medium.url}
                   alt="image video"
                />
-               <p className="video__title">{videoTitle}</p>
+               <ShowMoreText
+                  className="video__title"
+                  lines={2}
+                  more=""
+                  less="Show less"
+                  anchorClass="show-more-less-clickable"
+                  expanded={false}
+                  truncatedEndingComponent={'...'}
+               >
+                  <p className="video__title">{videoTitle}</p>
+               </ShowMoreText>
                <p className="video__title channel__title">
                   {video.snippet.channelTitle}
                </p>
