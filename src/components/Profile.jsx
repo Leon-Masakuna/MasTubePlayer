17ac26f9a456403f3c3208f7eb5f22c7596afe89
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-12-10 15:17:24 +0100

    feat : Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,73 +1,33 @@
-import React from 'react';
-import Logout from './Logout';
-import "../styles/profile_style.css"
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import React from 'react'
+import '../styles/profile_style.css'
+import { useState, useEffect } from 'react'
+import ProfileCard from './ProfileCard'
+import { Link, useParams } from 'react-router-dom'
 
 const Profile = () => {
-    const [userNames, setUserNames] = useState("");
-    const [userEmail, setUserEmail] = useState("");
-    const [userAvatar, setUserAvatar] = useState("");
-    const [userLinkedin, setUserLinkedin] = useState("");
-    const [userGithub, setUserGithub] = useState("")
+   const [user, setUser] = useState([])
+   const { userId } = useParams()
 
-    return (
-        <>
-            <div className='container'>
-                <div className='back-arrow'>
-                    <Link to={'/dashbord'} className="link">
-                        <i className="fa-solid fa-arrow-left"></i>{" "}{" Go back"}
-                    </Link>
-                </div>
-                <div className='logout-component'>
-                    <Logout />
-                </div>
-                <div className='edit-button'>
-                    <Link to={'/userprofiledit'} className="link">
-                        <i className="fa-solid fa-pen-to-square"></i> {" "}{" Edit"}
-                    </Link>
-                </div>
-            </div>
-            <div className='profile-container'>
-                <div className='profile-pictures'>
-                    <img
-                        src="https://ca.slack-edge.com/T03BH6JN601-U03FK6874CF-fb4094095857-512"
-                        alt="Léon Mfeng"
-                        className="profile-photo"
-                    />
-                </div>
-                <div className='profile-identity'>
-                    <div className='name'>
-                        <h2 className='user-name'>Léon MASAKUNA</h2>
-                    </div>
-                    <div className='row'>
-                        <h2>
-                            <i className="fa-solid fa-phone"></i>{" "} 
-                            <span>+243815670620</span>
-                        </h2>
-                    </div>
-                    <div className='row'>
-                        <h2>
-                            <i className="fa-sharp fa-solid fa-envelope"></i>{" "} 
-                            <span>masakunamfengleon@gmail.com</span>
-                        </h2>
-                    </div>
-                    <div className='row'>
-                        <h2>
-                            <i className="fa-brands fa-linkedin"></i>{" "}
-                            <span>Léon Masakuna Mfeng</span>
-                        </h2>
-                    </div>
-                    <div className='row'>
-                        <h2>
-                            <i class="fa-brands fa-square-github"></i> {" "}
-                            <span>Leon-Masakuna</span>
-                        </h2>
-                    </div>
-                </div>
-            </div>        
-        </>
-    );
-};
+   const fetchData = `http://localhost:8100/api/user/${userId}`
+   const accessToken = localStorage.getItem('token')
 
-export default Profile;
+   useEffect(() => {
+      fetch(fetchData, {
+         method: 'GET',
+         headers: new Headers({ Authorization: `Bearer ${accessToken}` }),
+      })
+         .then((res) => res.json())
+         .then((data) => {
+            setUser(data)
+            console.log('items: ', data.github)
+         })
+   }, [])
+
+   return (
+      <>
+         <ProfileCard user={user} />
+      </>
+   )
+}
+
+export default Profile
