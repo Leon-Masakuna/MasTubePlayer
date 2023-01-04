2088556ea08b84c0b1cee2e10083f2bbe1db5720
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-01-04 09:10:13 +0100

    feat : Like behavior

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from 'react'
+import socketIO from 'socket.io-client'
+import React, { useState, useEffect } from 'react'
 
-const Like = () => {
-   const [likeCount, setLikeCount] = useState(0)
-   const addLike = () => {
-      setLikeCount(likeCount + 1)
+const socket = socketIO.connect('http://localhost:8100')
+const Like = ({ comment }) => {
+   const [likeCounts, setLikeCounts] = useState([])
+
+   const Likes = likeCounts.filter(
+      (likeCount) => likeCount.commentId === comment._id
+   )
+
+   const getLikes = () => {
+      socket.emit('getLikes', {})
    }
+   //    console.log('likes : ', comment)
+
+   useEffect(() => {
+      getLikes()
+      socket.on('receiveLikes', (like) => {
+         setLikeCounts(like)
+      })
+      socket.on('likeResponse', (like) => setLikeCounts([like, ...likeCounts]))
+   }, [socket, likeCounts])
+
+   const addLike = () => {
+      socket.emit('likeSend', {
+         commentId: comment._id,
+         userId: localStorage.getItem('userId'),
+      })
+   }
+
    return (
       <div>
          <i className="fa-solid fa-thumbs-up" onClick={addLike}></i>
-         <span> {likeCount} </span>
+         <span> {Likes.length} </span>
       </div>
    )
 }
