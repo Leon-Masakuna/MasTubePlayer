0aceb6f1afabdc67577f8ce933a33b448f96dce9
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-01-09 16:30:20 +0100

    dislike a comment

--- src/components/Dislike.jsx
+++ src/components/Dislike.jsx
@@ -1,14 +1,42 @@
-import React, { useState } from 'react'
+import socketIO from 'socket.io-client'
+import React, { useState, useEffect } from 'react'
 
-const Dislike = () => {
-   const [dislikeCount, setDislikeCount] = useState(0)
+const socket = socketIO.connect('http://localhost:8100')
+const Dislike = ({ comment }) => {
+   const [dislikeCounts, setDislikeCounts] = useState([])
+
+   const Dislikes = dislikeCounts.filter(
+      (dislike) => dislike.commentId === comment._id
+   )
+
+   const getDislikes = () => {
+      socket.emit('getDislikes', {})
+   }
+
+   useEffect(() => {
+      getDislikes()
+      socket.on('receiveDislikes', (dislike) => {
+         setDislikeCounts(dislike)
+      })
+      socket.on('dislikeResponse', (dislike) =>
+         setDislikeCounts([dislike, ...dislikeCounts])
+      )
+   }, [socket, dislikeCounts])
+
    const addDislike = () => {
-      setDislikeCount(dislikeCount + 1)
+      socket.emit('dislikeSend', {
+         commentId: comment._id,
+         userId: localStorage.getItem('userId'),
+      })
+
+      /* socket.emit('dislikeDeleted', {
+         _id: comment._id,
+      }) */
    }
    return (
       <div>
          <i className="fa-solid fa-thumbs-down" onClick={addDislike}></i>
-         <span> {dislikeCount} </span>
+         <span> {Dislikes.length} </span>
       </div>
    )
 }
