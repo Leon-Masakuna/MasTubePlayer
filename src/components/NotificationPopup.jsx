287d5bf4e5e9f97deaf873bcdd7e68e4b6499f9e
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-01-09 16:32:24 +0100

    feat : show popup

--- src/components/NotificationPopup.jsx
+++ src/components/NotificationPopup.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import socketIO from 'socket.io-client'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import '../styles/notification.css'
 import Message from './Message'
 
-const NotificationPopup = ({ showPopUp }) => {
+const socket = socketIO.connect('http://localhost:8100')
+const NotificationPopup = ({ showPopUp, notifications }) => {
+   const [notificationRows, setNotificationRows] = useState([])
+
+   const getNotifications = () => {
+      socket.emit('getNotifications', {})
+   }
+
+   useEffect(() => {
+      getNotifications()
+      socket.on('receiveNotifications', (notification) => {
+         setNotificationRows(notification)
+      })
+      socket.on('notificationResponse', (notification) =>
+         setNotificationRows([notification, ...notifications])
+      )
+   }, [socket, notifications])
+
+   // console.log('popup : ', notificationRows)
+
    return (
       <div className="popup-container">
          <div className="overlay" onClick={showPopUp}></div>
@@ -12,10 +33,9 @@ const NotificationPopup = ({ showPopUp }) => {
                <i className="fa-solid fa-circle-xmark close-button"></i>
             </h1>
          </div>
-         <Message />
-         <Message />
-         <Message />
-         <Message />
+         {notificationRows.map((notification) => {
+            return <Message notification={notification} />
+         })}
       </div>
    )
 }
