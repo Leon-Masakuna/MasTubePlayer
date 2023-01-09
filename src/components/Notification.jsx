6d68757f574f816f32ce483f79bc5e6c2e2ad27c
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-01-09 16:31:56 +0100

    feat : get notification length

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,21 +1,41 @@
-import React, { useState } from 'react'
+import socketIO from 'socket.io-client'
+import React, { useEffect, useState } from 'react'
 import NotificationPopup from './NotificationPopup'
 
+const socket = socketIO.connect('http://localhost:8100')
 const Notification = () => {
-   const [count, setCount] = useState(4)
    const [modal, setModal] = useState(false)
+   const [notifications, setNotifications] = useState([])
 
    const showPopUp = () => {
       setModal(!modal)
    }
 
+   const getNotifications = () => {
+      socket.emit('getNotifications', {})
+   }
+   // console.log('notifications : ', notifications)
+
+   useEffect(() => {
+      getNotifications()
+      socket.on('receiveNotifications', (notification) => {
+         setNotifications(notification)
+      })
+      socket.on('notificationResponse', (notification) =>
+         setNotifications([notification, ...notifications])
+      )
+   }, [socket, notifications])
+
    return (
       <>
          <div className="notification" onClick={showPopUp}>
-            <i className="fa-sharp fa-solid fa-comment" onClick={showPopUp}></i>
-            <span className="counter">{count}</span>
+            <i className="fa-solid fa-bell fa-comment" onClick={showPopUp}></i>
+            <span className="counter">{notifications.length}</span>
          </div>
-         {modal && <NotificationPopup showPopUp={showPopUp} />}
+         {modal &&
+            notifications.map(() => {
+               return <NotificationPopup showPopUp={showPopUp} />
+            })}
       </>
    )
 }
