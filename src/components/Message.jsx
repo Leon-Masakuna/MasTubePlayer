dd527b190dbedf9d0601ee19d8a4be84d592b939
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-01-09 16:31:26 +0100

    feat : show notification msg

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,26 +1,29 @@
+import socketIO from 'socket.io-client'
 import React from 'react'
 import moment from 'moment/moment'
 
-const Message = () => {
+const socket = socketIO.connect('http://localhost:8100')
+const Message = ({ notification }) => {
+   // console.log('popup : ', notification)
+
    return (
       <div>
          <hr />
          <div className="popup-list">
             <div className="popup-msg-img">
                <img
-                  src={localStorage.getItem('imageUrl')}
+                  src={notification.userIdSenderImage}
                   alt="image"
                   className="popup-user-icon"
                />
             </div>
             <div className="popup-msg">
                <div className="popup-msg-text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Temporibus nihil reprehenderit ullam earum similique eum
-                  labore minima?
+                  {notification.userIdSenderName}
+                  {notification.description}
                </div>
                <div className="popup-msg-time">
-                  {moment(new Date().getTime()).fromNow()}
+                  {moment(notification.createdAt).fromNow()}
                </div>
             </div>
          </div>
